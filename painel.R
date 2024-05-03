76e67e5633f9c884356641923fc6e282d76740c5
Armando Nahmias <armando@informartizar.com>
2024-05-02 23:22:50 -0400

    Organização geral

--- painel.R
+++ painel.R
@@ -2,41 +2,40 @@
 endereco <-
   'https://docs.google.com/spreadsheets/d/1Nw9Xj8TcuwFxeTml8oE_fPH0-SXWjRQzOCJsP_t1sms/edit#gid=0'
 
-indicadores.1.instancia <-
+dados.1.instancia <-
   googlesheets4::read_sheet(endereco,
                             sheet = '1InsT')
 
-indicadores.2.instancia <-
+dados.2.instancia <-
   googlesheets4::read_sheet(endereco,
                             sheet = '2InsT')
 
-indicadores <- list('1InsT' = indicadores.1.instancia, '2InsT' = indicadores.2.instancia)
+dados.painel <- list('1InsT' = dados.1.instancia, '2InsT' = dados.2.instancia)
+readr::write_rds(dados.painel, 'dados/dados.painel.rds')
+
 
 unidades <- list(
-  '1InsT' = indicadores.1.instancia |> 
+  '1InsT' = dados.1.instancia |> 
     dplyr::filter(!is.na(Unidade)) |>
     dplyr::select(Id)|> 
     dplyr::pull(),
-  '2InsT' = indicadores.2.instancia |> 
+  '2InsT' = dados.2.instancia |> 
     dplyr::filter(!is.na(Unidade)) |>
     dplyr::select(Id)|> 
     dplyr::pull()
 )
 
-
 for (instancia in names(unidades)) {
-  nomes.indicadores <- colnames(indicadores[[instancia]])[-c(1,2)]
-
-  conteudo.base.qmd <- readLines('modelo/conteudo.base.qmd.qmd')
-  
+  nomes.indicadores <- colnames(dados.painel[[instancia]])[-c(1,2)]
   unidade.padrao <- unidades[[instancia]][1]
-  conteudo.base.qmd <- gsub('{{unidade.padrao}}', unidade.padrao, conteudo.base.qmd, fixed = TRUE)
+  
+  conteudo.base.qmd <- readLines('modelo/conteudo.base.qmd.qmd') |> 
+    stringi::stri_replace_all_fixed('{{unidade.padrao}}', unidade.padrao)
 
   for(indicador in nomes.indicadores) {
 
-    conteudo.variavel.qmd <- readLines('modelo/conteudo.variavel.qmd.qmd')
-
-    conteudo.variavel.qmd <- gsub('{{indicador}}', indicador, conteudo.variavel.qmd, fixed = TRUE)
+    conteudo.variavel.qmd <- readLines('modelo/conteudo.variavel.qmd.qmd') |> 
+      stringi::stri_replace_all_fixed('{{indicador}}', indicador)
 
     conteudo.base.qmd <- c(
       conteudo.base.qmd,
@@ -44,14 +43,14 @@ for (instancia in names(unidades)) {
     )
   }
   
-  readr::write_rds(indicadores[instancia], 'dados/Indicadores.rds')
+  readr::write_rds(dados.painel[instancia], 'dados/indicadores.rds')
   
   arquivo.destino <- glue::glue('painel_indicadores_{instancia}.qmd')
   
   writeLines(conteudo.base.qmd, arquivo.destino)
   
   parametros <- list(Id = unidades[[instancia]])
-  arquivos.saida <- paste0(unidades[[instancia]], '.html')
+  arquivos.saida <- paste0('relatorio_', unidades[[instancia]], '.html')
   diretorio.destino <- 'pagina/'
 
     
@@ -66,8 +65,10 @@ for (instancia in names(unidades)) {
   }
   
   file.rename(arquivo.destino, paste0(diretorio.destino, arquivo.destino))
-  
-  
-  
 
 }
+
+quarto::quarto_render(
+  input = 'modelo/index.qmd',
+  output_file = 'index.html'
+)
